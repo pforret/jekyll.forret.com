bacb57e6a14f63beada0497a1c60fffa46a9ea10
Peter Forret <peter@forret.com>
2022-01-18 20:34:00 +0100

    ADD: serve.sh,

---
+++ serve.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+[[ -z $(command -v jekyll) ]] && echo "Jekyll not installed on this system" && exit 1
+bundle exec jekyll build
+( sleep 5 && open http://localhost:4000 ) &
+bundle exec jekyll serve --incremental
