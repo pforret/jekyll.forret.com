c6e7363c4cb61222d5ca2376544290463ff446e0
Peter Forret <peter@forret.com>
2022-05-03 23:42:40 +0200

    ADD: carousel.html 2022-05-02-gdpr.md IMG_5428.PNG IMG_5429.PNG IMG_5430.PNG chrome_cookies.png unsplash.cookie.jpg unsplash.crash.jpg unsplash.dumpster.jpg unsplash.explosion.jpg unsplash.fire.jpg unsplash.pills.jpg unsplash.protection.jpg, MOD: serve.sh

--- serve.sh
+++ serve.sh
@@ -2,4 +2,4 @@
 
 [[ -z $(command -v jekyll) ]] && echo "Jekyll not installed on this system" && exit 1
 ( sleep 10 && open http://localhost:4000 ) &
-bundle exec jekyll serve --incremental
+bundle exec jekyll serve
